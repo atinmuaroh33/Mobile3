ef6192bb12531db5f78794718ab2738216a0cc1f
atinmuaroh33 <atinmuaroh33@gmail.com>
2022-09-27 21:51:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikum3/successlogin7.java
@@ -0,0 +1,15 @@
+package com.example.praktikum3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class successlogin7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_login7);
+    }
+}
+
